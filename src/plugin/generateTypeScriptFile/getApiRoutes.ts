8cc3c374e91ef5bd258d656bf041726ac09656f7
Christian Kastbjerg <christian@helloproper.com>
2021-03-21 22:12:36 +0100

    :fire: Remove console log

--- src/plugin/generateTypeScriptFile/getApiRoutes.ts
+++ src/plugin/generateTypeScriptFile/getApiRoutes.ts
@@ -5,7 +5,6 @@ import { ApiRoute } from "./types";
 const shouldIncludeEntry = (endpoint: string) => endpoint.match(".ts");
 
 const getApiRoutes = (fileNames: string[]): ApiRoute[] => {
-  console.log({ fileNames });
   return fileNames.filter(shouldIncludeEntry).map((fileName) => {
     const route = getNextPageRoute(fileName);
     const params = getNextRouteUrlParams(route);
