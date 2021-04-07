a6b2dd19c63999f15a178a5ffc5eca5a53195cb3
Christian Kastbjerg <christian@helloproper.com>
2021-04-07 14:07:09 +0200

    :white_check_mark: Update example project

--- example/src/pages/users/[userId].tsx
+++ example/src/pages/users/[userId].tsx
@@ -4,7 +4,11 @@ import { useApiRoute, useRouter } from "hooks";
 const UserPage = () => {
   const { query } = useRouter();
   const userId = query.userId as string;
-  const [user] = useApiRoute({ route: "/api/users/[userId]", userId });
+  const [user] = useApiRoute({
+    route: "/api/users/[userId]",
+    params: { userId },
+    query: { test: 2 },
+  });
 
   if (!user) {
     return "Loading...";
